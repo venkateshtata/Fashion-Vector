c36a2d1f61932b91425952d55b4288ab279d2a67
venkateshtata <venkateshtata9@gmail.com>
2021-10-24 09:53:02 +0530

    config update

--- unifiedAPI/producer_test.py
+++ unifiedAPI/producer_test.py
@@ -3,10 +3,7 @@ from vectorai import Unified
 kafka_config = {
 	'bootstrap_servers': ['localhost:9092'],
 	'topic': 'send_image',
-	'model_server': 'http://127.0.0.1:8080/predictions/fashion',
-	'group_id': 'consumer-group-a',
-	'predictions_topic': 'get_predictions',
-	'pred_group_id': 'pred_group'
+	'group_id': 'consumer-group-a'
 }
 
 google_config = {
